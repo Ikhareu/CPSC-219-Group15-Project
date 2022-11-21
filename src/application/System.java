143131b7b71ac8730a04df19e5864fa297872299
Ikhareu <67671177+Ikhareu@users.noreply.github.com>
2022-11-21 04:30:41 -0700

    saving progress.

---
+++ src/application/System.java
@@ -0,0 +1,32 @@
+package application;
+	
+import java.io.FileInputStream;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+
+public class System extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			Pane root = loader.load(new FileInputStream("src/view/Login Page.fxml"));
+			Scene scene = new Scene(root,400,350);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("House Renting System");
+			
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
