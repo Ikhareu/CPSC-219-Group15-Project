cff08a4aca0a9606c2909867486fbb0c847b3dd8
Булат <@10.12.189.167>
2022-11-24 13:14:46 -0700

    homepage version1 added

--- src/view/userhomepagecontroller.java
+++ src/view/userhomepagecontroller.java
@@ -6,4 +6,14 @@ public class userhomepagecontroller {
 	
 	
 
+	void register(actioneven) {
+		
+		login=label1.getvalue
+		password = label
+		
+		userRegistered(label, email,phone,id)
+		
+		userList.adduse(userRegistered)
+	}
+	
 }
