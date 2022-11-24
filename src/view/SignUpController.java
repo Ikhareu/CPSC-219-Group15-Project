5c4137fba47f39194bc04715eacd49313573772d
Ikhareu <67671177+Ikhareu@users.noreply.github.com>
2022-11-24 11:57:52 -0700

    controller class for sign up page.

---
+++ src/view/SignUpController.java
@@ -0,0 +1,24 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SignUpController {
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private TextField repeatPasswordTextfield;
+
+    @FXML
+    private TextField emailTextfield;
+
+    @FXML
+    private TextField passwordTextield;
+
+    @FXML
+    private Button signUp;
+
+}
