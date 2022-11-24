c884bacb715163d070d690e700d58a6e169f5e0e
AdamAzaiez <adam.azaiez@ucalgary.ca>
2022-11-24 18:19:53 +0000

    added stuff

--- src/objects/Users.java
+++ src/objects/Users.java
@@ -1,9 +1,18 @@
 package objects;
 
 public class Users {
+	private String phone = null;
+	private String email = null;
+	private String name = null;
 
 	public Users() {
-		// TODO Auto-generated constructor stub
-	}
+		//phone = fxid:inputPhone;
+		//email = fxid:inputEmail;
+		//name  = fxid:inputName;
+		
+		}
 
+ 	public static void addCourse(Course aCourse) {
+ 		
+ 	}
 }
