89ce6bbeedae125375038f108b605efca058c988
Ikhareu <67671177+Ikhareu@users.noreply.github.com>
2022-11-25 18:35:56 -0700

    Controller created for Login page.

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module OnlineHouseRentingSystem {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.base;
 
 	opens application to javafx.graphics, javafx.fxml;
 	opens view to javafx.graphics, javafx.fxml;
---
+++ src/view/LoginController2.java
@@ -0,0 +1,35 @@
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController2 {
+
+    @FXML
+    private TextField loginUsername;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private PasswordField loginPassword;
+
+    @FXML
+    private Hyperlink createAccountHyperlink;
+
+    @FXML
+    void loginAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signUpAction(ActionEvent event) {
+
+    }
+
+}
+
