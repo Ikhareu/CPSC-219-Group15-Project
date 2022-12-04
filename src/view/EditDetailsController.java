a5e88305ad0642a00e97b9c4d3582c6dfc57f0e5
Ikhareu <67671177+Ikhareu@users.noreply.github.com>
2022-12-04 15:01:34 -0700

    edit details page created.

---
+++ src/view/EditDetailsController.java
@@ -0,0 +1,36 @@
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class EditDetailsController {
+
+    @FXML
+    private TextField passwordText;
+
+    @FXML
+    private TextField usernameText;
+
+    @FXML
+    private TextField emailText;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private Label nameLabel;
+    
+    @FXML
+    void goBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveChanges(ActionEvent event) {
+
+    }
+
+}
