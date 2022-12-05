d031384ac763af8dd830b31dcd91cf9798b0b77e
Ikhareu <67671177+Ikhareu@users.noreply.github.com>
2022-12-04 22:19:29 -0700

    created tutors list class.

---
+++ src/objects/Tutors_List.java
@@ -0,0 +1,79 @@
+package objects;
+
+import java.util.ArrayList;
+
+public class Tutors_List {
+	private ArrayList<Tutor> CPSC_217 = new ArrayList<Tutor>();
+	private ArrayList<Tutor> CPSC_219 = new ArrayList<Tutor>();
+	private ArrayList<Tutor> CPSC_231 = new ArrayList<Tutor>();
+	private ArrayList<Tutor> CPSC_233 = new ArrayList<Tutor>();
+	private ArrayList<Tutor> CPSC_251 = new ArrayList<Tutor>();
+	private ArrayList<Tutor> CPSC_313 = new ArrayList<Tutor>();
+	private ArrayList<Tutor> CPSC_319 = new ArrayList<Tutor>();
+	private ArrayList<Tutor> CPSC_329 = new ArrayList<Tutor>();
+	private ArrayList<Tutor> CPSC_331 = new ArrayList<Tutor>();
+	private ArrayList<Tutor> CPSC_351 = new ArrayList<Tutor>();
+	
+	public ArrayList<Tutor> getCPSC_217() {
+		return CPSC_217;
+	}
+	public ArrayList<Tutor> getCPSC_219() {
+		return CPSC_219;
+	}
+	public ArrayList<Tutor> getCPSC_231() {
+		return CPSC_231;
+	}
+	public ArrayList<Tutor> getCPSC_233() {
+		return CPSC_233;
+	}
+	public ArrayList<Tutor> getCPSC_251() {
+		return CPSC_251;
+	}
+	public ArrayList<Tutor> getCPSC_313() {
+		return CPSC_313;
+	}
+	public ArrayList<Tutor> getCPSC_319() {
+		return CPSC_319;
+	}
+	public ArrayList<Tutor> getCPSC_329() {
+		return CPSC_329;
+	}
+	public ArrayList<Tutor> getCPSC_331() {
+		return CPSC_331;
+	}
+	public ArrayList<Tutor> getCPSC_351() {
+		return CPSC_351;
+	}
+	
+	public boolean add(Tutor e) {
+		return CPSC_217.add(e);
+	}
+	public boolean addCPSC_219(Tutor e) {
+		return CPSC_219.add(e);
+	}
+	public boolean addCPSC_231(Tutor e) {
+		return CPSC_231.add(e);
+	}
+	public boolean addCPSC_233(Tutor e) {
+		return CPSC_233.add(e);
+	}
+	public boolean addCPSC_251(Tutor e) {
+		return CPSC_251.add(e);
+	}
+	public boolean addCPSC_313(Tutor e) {
+		return CPSC_313.add(e);
+	}
+	public boolean addCPSC_319(Tutor e) {
+		return CPSC_319.add(e);
+	}
+	public boolean addCPSC_329(Tutor e) {
+		return CPSC_329.add(e);
+	}
+	public boolean addCPSC_331(Tutor e) {
+		return CPSC_331.add(e);
+	}
+	public boolean addCPSC_351(Tutor e) {
+		return CPSC_351.add(e);
+	}
+
+}
