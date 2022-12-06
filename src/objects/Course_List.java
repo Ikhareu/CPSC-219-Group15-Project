a48d5b2968fe67b89ebf4917e880aabe93d41f39
Ikhareu <67671177+Ikhareu@users.noreply.github.com>
2022-12-06 16:49:38 -0700

    generated course list

---
+++ src/objects/Course_List.java
@@ -0,0 +1,79 @@
+package objects;
+
+import java.util.ArrayList;
+
+public class Course_List {
+	
+	private static ArrayList<String> CPSC217 = new ArrayList<String>();
+	private static ArrayList<String> CPSC219 = new ArrayList<String>();
+	private static ArrayList<String> CPSC231 = new ArrayList<String>();
+	private static ArrayList<String> CPSC233 = new ArrayList<String>();
+	private static ArrayList<String> CPSC251 = new ArrayList<String>();
+	private static ArrayList<String> CPSC313 = new ArrayList<String>();
+	private static ArrayList<String> CPSC319 = new ArrayList<String>();
+	private static ArrayList<String> CPSC329 = new ArrayList<String>();
+	private static ArrayList<String> CPSC331 = new ArrayList<String>();
+	private static ArrayList<String> CPSC351 = new ArrayList<String>();
+	
+	public boolean add217(String e) {
+		return CPSC217.add(e);
+	}
+	public boolean add219(String e) {
+		return CPSC219.add(e);
+	}
+	public boolean add231(String e) {
+		return CPSC231.add(e);
+	}
+	public boolean add233(String e) {
+		return CPSC233.add(e);
+	}
+	public boolean add251(String e) {
+		return CPSC251.add(e);
+	}
+	public boolean add313(String e) {
+		return CPSC313.add(e);
+	}
+	public boolean add319(String e) {
+		return CPSC319.add(e);
+	}
+	public boolean add329(String e) {
+		return CPSC329.add(e);
+	}
+	public boolean add331(String e) {
+		return CPSC331.add(e);
+	}
+	public boolean add351(String e) {
+		return CPSC351.add(e);
+	}
+	public static ArrayList<String> getCPSC217() {
+		return CPSC217;
+	}
+	public static ArrayList<String> getCPSC219() {
+		return CPSC219;
+	}
+	public static ArrayList<String> getCPSC231() {
+		return CPSC231;
+	}
+	public static ArrayList<String> getCPSC233() {
+		return CPSC233;
+	}
+	public static ArrayList<String> getCPSC251() {
+		return CPSC251;
+	}
+	public static ArrayList<String> getCPSC313() {
+		return CPSC313;
+	}
+	public static ArrayList<String> getCPSC319() {
+		return CPSC319;
+	}
+	public static ArrayList<String> getCPSC331() {
+		return CPSC331;
+	}
+	public static ArrayList<String> getCPSC329() {
+		return CPSC329;
+	}
+	public static ArrayList<String> getCPSC351() {
+		return CPSC351;
+	}
+
+}
